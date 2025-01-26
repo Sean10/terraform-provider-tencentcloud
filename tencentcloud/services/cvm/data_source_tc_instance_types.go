3e31b0d3a5cd7657a42cfda1841c45a9892d47c6
sean10 <sean10reborn@gmail.com>
2025-01-26 22:01:34 +0800

    DataSourceTencentCloudInstanceTypes support sort by UnitPriceDiscount for SPOTPAID

--- tencentcloud/services/cvm/data_source_tc_instance_types.go
+++ tencentcloud/services/cvm/data_source_tc_instance_types.go
@@ -3,19 +3,19 @@ package cvm
 import (
 	"context"
 	"log"
+	"sort"
 
-	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
-
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	cvm "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/cvm/v20170312"
-
+	tccommon "github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/common"
 	"github.com/tencentcloudstack/terraform-provider-tencentcloud/tencentcloud/internal/helper"
 )
 
 func DataSourceTencentCloudInstanceTypes() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceTencentCloudInstanceTypesRead,
+		ReadContext: dataSourceTencentCloudInstanceTypesRead,
 
 		Schema: map[string]*schema.Schema{
 			"cpu_core_count": {
@@ -39,6 +39,12 @@ func DataSourceTencentCloudInstanceTypes() *schema.Resource {
 				ConflictsWith: []string{"filter"},
 				Description:   "The available zone that the CVM instance locates at. This field is conflict with `filter`.",
 			},
+			"exclude_sold_out": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Indicate to filter instances types that is sold out or not, default is false.",
+			},
 			"filter": {
 				Type:          schema.TypeSet,
 				Optional:      true,
@@ -61,19 +67,11 @@ func DataSourceTencentCloudInstanceTypes() *schema.Resource {
 					},
 				},
 			},
-			"exclude_sold_out": {
-				Type:        schema.TypeBool,
-				Optional:    true,
-				Default:     false,
-				Description: "Indicate to filter instances types that is sold out or not, default is false.",
-			},
 			"result_output_file": {
 				Type:        schema.TypeString,
 				Optional:    true,
 				Description: "Used to save results.",
 			},
-
-			// Computed values.
 			"instance_types": {
 				Type:        schema.TypeList,
 				Computed:    true,
@@ -120,6 +118,11 @@ func DataSourceTencentCloudInstanceTypes() *schema.Resource {
 							Computed:    true,
 							Description: "Sell status of the instance.",
 						},
+						"price": {
+							Type:        schema.TypeFloat,
+							Computed:    true,
+							Description: "Instance price per hour.",
+						},
 					},
 				},
 			},
@@ -127,98 +130,149 @@ func DataSourceTencentCloudInstanceTypes() *schema.Resource {
 	}
 }
 
-func dataSourceTencentCloudInstanceTypesRead(d *schema.ResourceData, meta interface{}) error {
+type instanceTypeSort struct {
+	instanceTypes []*cvm.InstanceTypeQuotaItem
+}
+
+func (s instanceTypeSort) Len() int {
+	return len(s.instanceTypes)
+}
+
+func (s instanceTypeSort) Swap(i, j int) {
+	s.instanceTypes[i], s.instanceTypes[j] = s.instanceTypes[j], s.instanceTypes[i]
+}
+
+func (s instanceTypeSort) Less(i, j int) bool {
+	// 按照价格从低到高排序
+	if s.instanceTypes[i].Price != nil && s.instanceTypes[j].Price != nil {
+		return *s.instanceTypes[i].Price.UnitPriceDiscount < *s.instanceTypes[j].Price.UnitPriceDiscount
+	}
+	return false
+}
+
+func dataSourceTencentCloudInstanceTypesRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
 	defer tccommon.LogElapsed("data_source.tencentcloud_instance_types.read")()
-	logId := tccommon.GetLogId(tccommon.ContextNil)
-	ctx := context.WithValue(context.TODO(), tccommon.LogIdKey, logId)
+	logId := tccommon.GetLogId(ctx)
+	ctx = context.WithValue(ctx, tccommon.LogIdKey, logId)
+
 	cvmService := CvmService{
 		client: meta.(tccommon.ProviderMeta).GetAPIV3Conn(),
 	}
 
-	isExcludeSoldOut := d.Get("exclude_sold_out").(bool)
-	cpu, cpuOk := d.GetOk("cpu_core_count")
-	gpu, gpuOk := d.GetOk("gpu_core_count")
-	memory, memoryOk := d.GetOk("memory_size")
-	var instanceSellTypes []*cvm.InstanceTypeQuotaItem
-	var errRet error
-	var err error
-	typeList := make([]map[string]interface{}, 0)
-	ids := make([]string, 0)
-
-	var zone string
-	var zone_in = 0
-	if v, ok := d.GetOk("availability_zone"); ok {
-		zone = v.(string)
-		zone_in = 1
-	}
-	filters := d.Get("filter").(*schema.Set).List()
-	filterMap := make(map[string][]string, len(filters)+zone_in)
-	for _, v := range filters {
-		item := v.(map[string]interface{})
-		name := item["name"].(string)
-		values := item["values"].([]interface{})
-		filterValues := make([]string, 0, len(values))
-		for _, value := range values {
-			filterValues = append(filterValues, value.(string))
+	filter := make(map[string][]string)
+	if v, ok := d.GetOk("filter"); ok {
+		for _, v := range v.(*schema.Set).List() {
+			item := v.(map[string]interface{})
+			name := item["name"].(string)
+			values := helper.InterfacesStrings(item["values"].([]interface{}))
+			filter[name] = values
 		}
-		filterMap[name] = filterValues
 	}
-	if zone != "" {
-		filterMap["zone"] = []string{zone}
+
+	// 处理可用区参数
+	if v, ok := d.GetOk("availability_zone"); ok {
+		filter["zone"] = []string{v.(string)}
 	}
-	err = resource.Retry(tccommon.ReadRetryTimeout, func() *resource.RetryError {
-		instanceSellTypes, errRet = cvmService.DescribeInstancesSellTypeByFilter(ctx, filterMap)
+
+	var instanceTypes []*cvm.InstanceTypeQuotaItem
+	var errRet error
+
+	err := resource.Retry(tccommon.ReadRetryTimeout, func() *resource.RetryError {
+		instanceTypes, errRet = cvmService.DescribeInstancesSellTypeByFilter(ctx, filter)
 		if errRet != nil {
 			return tccommon.RetryError(errRet, tccommon.InternalError)
 		}
 		return nil
 	})
 	if err != nil {
-		return err
+		return diag.FromErr(err)
 	}
-	for _, instanceType := range instanceSellTypes {
-		flag := true
-		if cpuOk && int64(cpu.(int)) != *instanceType.Cpu {
-			flag = false
-		}
-		if gpuOk && int64(gpu.(int)) != *instanceType.Gpu {
-			flag = false
-		}
-		if memoryOk && int64(memory.(int)) != *instanceType.Memory {
-			flag = false
-		}
-		if isExcludeSoldOut && CVM_SOLD_OUT_STATUS == *instanceType.Status {
-			flag = false
-		}
 
-		if flag {
-			mapping := map[string]interface{}{
-				"availability_zone":    instanceType.Zone,
-				"cpu_core_count":       instanceType.Cpu,
-				"gpu_core_count":       instanceType.Gpu,
-				"memory_size":          instanceType.Memory,
-				"family":               instanceType.InstanceFamily,
-				"instance_type":        instanceType.InstanceType,
-				"instance_charge_type": instanceType.InstanceChargeType,
-				"status":               instanceType.Status,
+	// 过滤掉已售罄的实例类型
+	if v, ok := d.GetOkExists("exclude_sold_out"); ok && v.(bool) {
+		var available []*cvm.InstanceTypeQuotaItem
+		for _, instanceType := range instanceTypes {
+			if instanceType.Status != nil && *instanceType.Status == "SELL" {
+				available = append(available, instanceType)
 			}
-			typeList = append(typeList, mapping)
-			ids = append(ids, *instanceType.InstanceType)
 		}
+		instanceTypes = available
+	}
+
+	// 按照CPU、GPU和内存过滤
+	if v, ok := d.GetOk("cpu_core_count"); ok {
+		cpuCount := v.(int)
+		var matched []*cvm.InstanceTypeQuotaItem
+		for _, instanceType := range instanceTypes {
+			if instanceType.Cpu != nil && int(*instanceType.Cpu) == cpuCount {
+				matched = append(matched, instanceType)
+			}
+		}
+		instanceTypes = matched
+	}
+
+	if v, ok := d.GetOk("gpu_core_count"); ok {
+		gpuCount := v.(int)
+		var matched []*cvm.InstanceTypeQuotaItem
+		for _, instanceType := range instanceTypes {
+			if instanceType.Gpu != nil && int(*instanceType.Gpu) == gpuCount {
+				matched = append(matched, instanceType)
+			}
+		}
+		instanceTypes = matched
+	}
+
+	if v, ok := d.GetOk("memory_size"); ok {
+		memSize := v.(int)
+		var matched []*cvm.InstanceTypeQuotaItem
+		for _, instanceType := range instanceTypes {
+			if instanceType.Memory != nil && int(*instanceType.Memory) == memSize {
+				matched = append(matched, instanceType)
+			}
+		}
+		instanceTypes = matched
+	}
+
+	// 按价格排序
+	sort.SliceStable(instanceTypes, func(i, j int) bool {
+		if instanceTypes[i].Price != nil && instanceTypes[j].Price != nil {
+			return *instanceTypes[i].Price.UnitPriceDiscount < *instanceTypes[j].Price.UnitPriceDiscount
+		}
+		return false
+	})
+
+	instanceTypeList := make([]map[string]interface{}, 0, len(instanceTypes))
+	ids := make([]string, 0, len(instanceTypes))
+	for _, instanceType := range instanceTypes {
+		mapping := map[string]interface{}{
+			"availability_zone":    helper.PString(instanceType.Zone),
+			"instance_type":        helper.PString(instanceType.InstanceType),
+			"cpu_core_count":       helper.PInt64(instanceType.Cpu),
+			"gpu_core_count":       helper.PInt64(instanceType.Gpu),
+			"memory_size":          helper.PInt64(instanceType.Memory),
+			"family":               helper.PString(instanceType.InstanceFamily),
+			"instance_charge_type": helper.PString(instanceType.InstanceChargeType),
+			"status":               helper.PString(instanceType.Status),
+		}
+		if instanceType.Price != nil && instanceType.Price.UnitPriceDiscount != nil {
+			mapping["price"] = *instanceType.Price.UnitPriceDiscount
+		}
+		instanceTypeList = append(instanceTypeList, mapping)
+		ids = append(ids, *instanceType.InstanceType)
 	}
 
 	d.SetId(helper.DataResourceIdsHash(ids))
-	err = d.Set("instance_types", typeList)
-	if err != nil {
-		log.Printf("[CRITAL]%s provider set instance type list fail, reason:%s\n ", logId, err.Error())
-		return err
+	if e := d.Set("instance_types", instanceTypeList); e != nil {
+		log.Printf("[CRITAL]%s provider set instance types list fail, reason:%s\n", logId, e.Error())
+		return diag.FromErr(e)
 	}
 
 	output, ok := d.GetOk("result_output_file")
 	if ok && output.(string) != "" {
-		if err := tccommon.WriteToFile(output.(string), typeList); err != nil {
-			return err
+		if e := tccommon.WriteToFile(output.(string), instanceTypeList); e != nil {
+			return diag.FromErr(e)
 		}
 	}
+
 	return nil
 }
